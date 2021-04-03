ef5706899c8e27c95417d9be7e5ab6cbb9328d6d
Nitesh Agrawal <nit.agrawal@sap.com>
2021-04-04 00:46:37 +0530

    changes

--- data.js
+++ data.js
@@ -46,48 +46,48 @@ const data =  [
 			]
 		},
 		{
-			"date": "01-04-2021",
-			"match" : "DD,MI",
+			"date": "10-04-2021",
+			"match" : "CSK,DC",
 			"teams": [
-			{
-				    "sno": 0,
+				{
+					"sno": 0,
 					"player": "Nitesh",
-					"highRankTeam": "DD",
-					"lowRankTeam": "MI",
-					"highTeamPoint": 6,
+					"highRankTeam": "DC",
+					"lowRankTeam": "CSK",
+					"highTeamPoint": 8,
 					"lowTeamPoint": 4
 				},
 				{
 					"sno": 1,
 					"player": "Adarsh",
-					"highRankTeam": "MI",
-					"lowRankTeam": "DD",
-					"highTeamPoint": 4,
-					"lowTeamPoint": 2
+					"highRankTeam": "DC",
+					"lowRankTeam": "CSK",
+					"highTeamPoint": 12,
+					"lowTeamPoint": 6
 				},
 				{
 					"sno": 2,
 					"player": "Kinshuk",
-					"highRankTeam": "DD",
-					"lowRankTeam": "MI",
-					"highTeamPoint": 8,
-					"lowTeamPoint": 2
+					"highRankTeam": "DC",
+					"lowRankTeam": "CSK",
+					"highTeamPoint": 16,
+					"lowTeamPoint": 8
 				},
 				{
 					"sno": 3,
 					"player": "Pallu",
-					"highRankTeam": "MI",
-					"lowRankTeam": "DD",
-					"highTeamPoint": 5,
-					"lowTeamPoint": 3
+					"highRankTeam": "DC",
+					"lowRankTeam": "CSK",
+					"highTeamPoint": 12,
+					"lowTeamPoint": 6
 				},
 				{
 					"sno": 4,
 					"player": "Pandey",
-					"highRankTeam": "DD",
-					"lowRankTeam": "MI",
-					"highTeamPoint": 7,
-					"lowTeamPoint": 2
+					"highRankTeam": "CSK",
+					"lowRankTeam": "DC",
+					"highTeamPoint": 8,
+					"lowTeamPoint": 4
 				}
 			]
 		}
