63501b50fa7f1c7a3d1193d75eaaf7332c34767f
Nitesh Agrawal <nit.agrawal@sap.com>
2021-04-10 16:40:29 +0530

    changes

--- data.js
+++ data.js
@@ -90,6 +90,190 @@ const data =  [
 					"lowTeamPoint": 6
 				}
 			]
+		},
+		{
+			"date": "11-04-2021",
+			"match" : "SRH,KKR",
+			"teams": [
+				{
+					"sno": 0,
+					"player": "Nitesh",
+					"highRankTeam": "SRH",
+					"lowRankTeam": "KKR",
+					"highTeamPoint": 6,
+					"lowTeamPoint": 1
+				},
+				{
+					"sno": 1,
+					"player": "Adarsh",
+					"highRankTeam": "SRH",
+					"lowRankTeam": "KKR",
+					"highTeamPoint": 6,
+					"lowTeamPoint": 4
+				},
+				{
+					"sno": 2,
+					"player": "Kinshuk",
+					"highRankTeam": "SRH",
+					"lowRankTeam": "KKR",
+					"highTeamPoint": 12,
+					"lowTeamPoint": 1
+				},
+				{
+					"sno": 3,
+					"player": "Pallu",
+					"highRankTeam": "KKR",
+					"lowRankTeam": "SRH",
+					"highTeamPoint": 6,
+					"lowTeamPoint": 4
+				},
+				{
+					"sno": 4,
+					"player": "Pandey",
+					"highRankTeam": "KKR",
+					"lowRankTeam": "SRH",
+					"highTeamPoint": 8,
+					"lowTeamPoint": 1
+				}
+			]
+		},
+		{
+			"date": "12-04-2021",
+			"match" : "RR,PBKS",
+			"teams": [
+				{
+					"sno": 0,
+					"player": "Nitesh",
+					"highRankTeam": "PKBS",
+					"lowRankTeam": "RR",
+					"highTeamPoint": 8,
+					"lowTeamPoint": 2
+				},
+				{
+					"sno": 1,
+					"player": "Adarsh",
+					"highRankTeam": "PKBS",
+					"lowRankTeam": "RR",
+					"highTeamPoint": 2,
+					"lowTeamPoint": 1
+				},
+				{
+					"sno": 2,
+					"player": "Kinshuk",
+					"highRankTeam": "PKBS",
+					"lowRankTeam": "RR",
+					"highTeamPoint": 6,
+					"lowTeamPoint": 2
+				},
+				{
+					"sno": 3,
+					"player": "Pallu",
+					"highRankTeam": "PKBS",
+					"lowRankTeam": "RR",
+					"highTeamPoint": 8,
+					"lowTeamPoint": 2
+				},
+				{
+					"sno": 4,
+					"player": "Pandey",
+					"highRankTeam": "PKBS",
+					"lowRankTeam": "RR",
+					"highTeamPoint": 20,
+					"lowTeamPoint": 4
+				}
+			]
+		},
+		{
+			"date": "13-04-2021",
+			"match" : "KKR,MI",
+			"teams": [
+				{
+					"sno": 0,
+					"player": "Nitesh",
+					"highRankTeam": "CSK",
+					"lowRankTeam": "DC",
+					"highTeamPoint": 12,
+					"lowTeamPoint": 4
+				},
+				{
+					"sno": 1,
+					"player": "Adarsh",
+					"highRankTeam": "CSK",
+					"lowRankTeam": "DC",
+					"highTeamPoint": 20,
+					"lowTeamPoint": 12
+				},
+				{
+					"sno": 2,
+					"player": "Kinshuk",
+					"highRankTeam": "DC",
+					"lowRankTeam": "CSK",
+					"highTeamPoint": 20,
+					"lowTeamPoint": 4
+				},
+				{
+					"sno": 3,
+					"player": "Pallu",
+					"highRankTeam": "CSK",
+					"lowRankTeam": "DC",
+					"highTeamPoint": 16,
+					"lowTeamPoint": 1
+				},
+				{
+					"sno": 4,
+					"player": "Pandey",
+					"highRankTeam": "DC",
+					"lowRankTeam": "CSK",
+					"highTeamPoint": 16,
+					"lowTeamPoint": 6
+				}
+			]
+		},
+		{
+			"date": "14-04-2021",
+			"match" : "SRH,RCB",
+			"teams": [
+				{
+					"sno": 0,
+					"player": "Nitesh",
+					"highRankTeam": "CSK",
+					"lowRankTeam": "DC",
+					"highTeamPoint": 12,
+					"lowTeamPoint": 4
+				},
+				{
+					"sno": 1,
+					"player": "Adarsh",
+					"highRankTeam": "CSK",
+					"lowRankTeam": "DC",
+					"highTeamPoint": 20,
+					"lowTeamPoint": 12
+				},
+				{
+					"sno": 2,
+					"player": "Kinshuk",
+					"highRankTeam": "DC",
+					"lowRankTeam": "CSK",
+					"highTeamPoint": 20,
+					"lowTeamPoint": 4
+				},
+				{
+					"sno": 3,
+					"player": "Pallu",
+					"highRankTeam": "CSK",
+					"lowRankTeam": "DC",
+					"highTeamPoint": 16,
+					"lowTeamPoint": 1
+				},
+				{
+					"sno": 4,
+					"player": "Pandey",
+					"highRankTeam": "DC",
+					"lowRankTeam": "CSK",
+					"highTeamPoint": 16,
+					"lowTeamPoint": 6
+				}
+			]
 		}
 	]
 
